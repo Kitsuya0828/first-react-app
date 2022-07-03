9475a94cf49d8b00f5e628848b4e689e2bb1e63e
Kitsuya0828 <kitsuyaazuma@gmail.com>
2022-07-03 23:45:50 +0900

    change url

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 function App() {
 	const [data, setData] = React.useState();
-	const url = "https://shrouded-scrubland-12657.herokuapp.com/";
+	const url = "https://shrouded-scrubland-12657.herokuapp.com";
 
 	const GetData = () => {
 		axios.get(url).then((res) => {
